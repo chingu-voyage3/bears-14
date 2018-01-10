eeefb0ca3e5fea4d9b660d5d58a63bcc605ad53a
Stefan <stefantrysil@gmail.com>
2018-01-10 18:26:58 +0100

    server(feat) add ability to query for all events, move some auth routes to api routes

---
+++ server/src/routes/apiRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const mongoose = require('mongoose');
+
+const Event = mongoose.model('Event');
+
+const requireLogin = require('../middlewares/requireLogin');
+
+// get all events
+router.get('/events', async (req, res, next) => {
+  try {
+    const events = await Event.find({});
+    res.send(events);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// logout user
+router.get('/logout', requireLogin, (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// get user info
+router.get('/user', (req, res) => {
+  res.send(req.user);
+});
+
+module.exports = router;
